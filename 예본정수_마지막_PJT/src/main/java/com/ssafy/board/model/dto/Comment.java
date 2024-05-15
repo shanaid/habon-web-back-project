6b0f9de73100c441ede252bea08cc26cf6ff6ce7
하정수 <kmk2528@naver.com>
2024-05-15 22:58:12 +0900

    댓글 dto 수정

--- 예본정수_마지막_PJT/src/main/java/com/ssafy/board/model/dto/Comment.java
+++ 예본정수_마지막_PJT/src/main/java/com/ssafy/board/model/dto/Comment.java
@@ -9,5 +9,73 @@ public class Comment {
 	private int boardId; // 댓글이 작성된 게시판 id
 	private String comments; // 댓글내용
 	private String registDate; // 댓글 작성시간
-	
+	private int good; // 추천갯수
+	private int bad; // 비추갯수
+
+	public Comment(String comments) {
+		super();
+		this.comments = comments;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public int getBoardId() {
+		return boardId;
+	}
+
+	public void setBoardId(int boardId) {
+		this.boardId = boardId;
+	}
+
+	public String getComments() {
+		return comments;
+	}
+
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+
+	public String getRegistDate() {
+		return registDate;
+	}
+
+	public void setRegistDate(String registDate) {
+		this.registDate = registDate;
+	}
+
+	public int getGood() {
+		return good;
+	}
+
+	public void setGood(int good) {
+		this.good = good;
+	}
+
+	public int getBad() {
+		return bad;
+	}
+
+	public void setBad(int bad) {
+		this.bad = bad;
+	}
+
+	@Override
+	public String toString() {
+		return "Comment [id=" + id + ", userId=" + userId + ", boardId=" + boardId + ", comments=" + comments
+				+ ", registDate=" + registDate + ", good=" + good + ", bad=" + bad + "]";
+	}
 }
